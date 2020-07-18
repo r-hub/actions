cd5e718ed78a673832ed3d7fa2b377db8d4ab108
Gábor Csárdi <csardi.gabor@gmail.com>
2020-07-18 14:03:48 +0100

    First version of the pak builder action (incomplete)

---
+++ pak-builder/lib/build-macos.js
@@ -0,0 +1,28 @@
+
+const execa = require('execa');
+const installer = require('install-rstats');
+const install_libcurl = require('./lib/install-libcurl');
+const clone_pak = require('./lib/clone-pak');
+const build_pak = require('./lib/build-pak');
+
+async function build_macos(rversions) {
+    console.log('::group::Installing R versions: ' + rversions.join(', '));
+    await installer.install(rversions);
+    console.log('::endgroup::')
+
+    console.log('::group::Installing static libcurl from brew');
+    await install_libcurl();
+    console.log('::endgroup::')
+
+    console.log('::group::Getting pak from GitHub');
+    await clone_pak();
+    console.log('::endgroup::')
+
+    rversions.forEach(async function(ver) {
+        console.log('::group::Bulding pak for R ' + ver);
+        await build_pak(ver);
+        console.log('::endgroup::');
+    });
+}
+
+module.exports = build_macos;
