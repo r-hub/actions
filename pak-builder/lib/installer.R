8373825f3a75cfd25ed2cddb6238ca6f7bf54c25
Gábor Csárdi <csardi.gabor@gmail.com>
2020-07-19 00:35:35 +0100

    No cli before we got pkgdepends...

--- pak-builder/lib/installer.R
+++ pak-builder/lib/installer.R
@@ -11,7 +11,8 @@ mkdirp <- function(x) {
 }
 
 install_pkgdepends <- function() {
-    cli::cli_h2("Installing pkgdepends")
+    # No cli just yet
+    message("Installing pkgdepends")
     options(repos = c(CRAN = "https://cloud.r-project.org"))
     source("https://install-github.me/r-lib/pkgdepends")
 }
