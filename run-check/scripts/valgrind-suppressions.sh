35e4b2d243f20db657183cc18c5e9e76b5b094be
Gábor Csárdi <csardi.gabor@gmail.com>
2025-04-26 16:27:06 +0200

    Fix typo in valgrind pre script

--- run-check/scripts/valgrind-suppressions.sh
+++ run-check/scripts/valgrind-suppressions.sh
@@ -4,10 +4,10 @@
 # add them to the suppressions file
 
 if [[ -f /usr/libexec/valgrind/default.supp ]]; then
-  if [[ -f tools/valgrind.supp ]; then
-    cat tools/valgrind.supp >> /usr/libexec/valgrind/default.supp
-  fi
-  if [[ -f inst/valgrind.supp ]; then
-    cat inst/valgrind.supp >> /usr/libexec/valgrind/default.supp
-  fi
+    if [[ -f tools/valgrind.supp ]]; then
+	cat tools/valgrind.supp >> /usr/libexec/valgrind/default.supp
+    fi
+    if [[ -f inst/valgrind.supp ]]; then
+	cat inst/valgrind.supp >> /usr/libexec/valgrind/default.supp
+    fi
 fi
